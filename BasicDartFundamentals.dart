c5ed790a6aa150330097a2f163abf69f4769b5f2
Jedrick28 <jedricktenedero49@gmail.com>
2025-08-04 08:24:13 +0000

    Activity 1 Aug 4

--- BasicDartFundamentals.dart
+++ BasicDartFundamentals.dart
@@ -1,1 +1,27 @@
-text
+Dynamic - if you dont know if its string or integer
+
+void main(){
+    dynamic name = "Dart";
+    name = 42;
+    print ("Hello $name!");
+}
+
+void main () {
+  double result = calculateArea (12.5,5,false);
+  
+    print('The Resul for a rectangle is ' + result.toString());
+   result = calculateArea(12,5, true);
+    print("The result for a triangle is " + result.toString());
+
+}
+
+double calculateArea(double width, double height, bool isTriangle) {
+  double area;
+  if (isTriangle){
+    area = width * height / 2;
+  }else {
+    area = width * height;
+  }
+  
+  return area;
+}
